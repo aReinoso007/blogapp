683843b1b4ccd813fbf94e46166015c085e41853
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-03-11 15:48:15 -0500

    getting info data

--- src/pages/BlogPost.js
+++ src/pages/BlogPost.js
@@ -4,14 +4,32 @@ import EmptyList from '../components/EmptyList';
 import '../index.css';
 import { Link } from 'react-router-dom';
 
-const Blog = () => {
+const Blog = ({content}) => {
  
   return (
     <>
-      <Link className='blog-goBack' to='/'>
-        <span> &#8592;</span> <span>Go Back</span>
-      </Link>
-    </>
+    <Link className='blog-goBack' to='/'>
+      <span> &#8592;</span> <span>Go Back</span>
+    </Link>
+    {content ? (
+      <div className='blog-wrap'>
+        <header>
+          <p className='blog-date'>Published {content.createdAt}</p>
+          <h1>{content.title}</h1>
+          <div className='blog-subCategory'>
+            
+              <div>
+                <Chip label={content.title} />
+              </div>
+            
+          </div>
+        </header>
+        <div className='blog-content' dangerouslySetInnerHTML={{__html: content.content}}></div>
+      </div>
+    ) : (
+      <EmptyList />
+    )}
+  </>
   );
 };
 export default Blog;
