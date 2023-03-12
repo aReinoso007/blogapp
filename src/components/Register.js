93c00a8571dd0d9bc2a3785a752913fe31fec4c1
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-03-11 22:36:29 -0500

    reqquired

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,104 +3,109 @@ import axios from "axios";
 import "../styles/Register.css"
 
 class Register extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      name: "",
-      lastName: "",
-      username: "",
-      email: "",
-      password: "",
-    };
+        this.state = {
+            name: "",
+            lastName: "",
+            username: "",
+            email: "",
+            password: "",
+        };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  handleChange(event) {
-    const { name, value } = event.target;
+    handleChange(event) {
+        const { name, value } = event.target;
 
-    this.setState({
-      [name]: value,
-    });
-  }
+        this.setState({
+            [name]: value,
+        });
+    }
 
-  handleSubmit(event) {
-    event.preventDefault();
+    handleSubmit(event) {
+        event.preventDefault();
 
-    axios
-      .post("http://localhost:500/api/v1/blog/user/registro", this.state)
-      .then((response) => {
-        console.log(response.data);
-        if(response.status === 201){
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+        axios
+            .post("http://localhost:500/api/v1/blog/user/registro", this.state)
+            .then((response) => {
+                console.log(response.data);
+                if (response.status === 201) {
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
-  render() {
-    return (
-      <div className="register-container">
-        <h2>Register</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Last Name:
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Username:
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <button type="submit">Register</button>
-        </form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <section className="register-container">
+                <h2>Register</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            name="name"
+                            value={this.state.name}
+                            required
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        Last Name:
+                        <input
+                            type="text"
+                            name="lastName"
+                            required
+                            value={this.state.lastName}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        Username:
+                        <input
+                            type="text"
+                            name="username"
+                            required
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        Email:
+                        <input
+                            type="email"
+                            name="email"
+                            required
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <br />
+                    <label>
+                        Password:
+                        <input
+                            type="password"
+                            name="password"
+                            required
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <br />
+                    <button type="submit">Register</button>
+                </form>
+            </section>
+        );
+    }
 }
 
 export default Register;
