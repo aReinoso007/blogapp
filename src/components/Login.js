fdde66acef8161a2958821c7eadba79d73c78b68
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-03-11 23:53:25 -0500

    login as const

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,83 +1,96 @@
-import React, { Component } from "react";
+import { useRef, useState, useEffect, useContext } from 'react';
 import "../styles/Login.css"
 import { Link } from "react-router-dom";
 import { signIn } from "../config/Api";
-class Login extends Component {
+import AuthContext from '../context/AuthProvider';
+const Login = () => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-        };
+    const { setAuth } = useContext(AuthContext);
 
-        this.handleSubmit = this.handleSubmit.bind(this);
+    const emailRef = useRef();
+    const errRef = useRef();
+
+    const [email, setEmail] = useState('');
+    const [password, setpassword] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        emailRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [email, password])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            signIn({ email: email, password: password }).then((res) => {
+                console.log('res ', res);
+                setAuth({ email, password });
+                setEmail('');
+                setpassword('');
+                setSuccess(true);
+            })
+        } catch (err) {
+
+        }
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
-        signIn(this.state).then((res)=>{
-            if(res.status === 200){
-                //go to home
-            }else{
-                //show error message
-            }
-        })
-    }
-
-    render() {
-        return (
-            <>
-                {this.state.success ?
-                    (
-                        <section>
-                            <h1>Has iniciado sesion!</h1>
-                            <br />
+    return (
+        <>
+            {success ?
+                (
+                    <section>
+                        <h1>Has iniciado sesion!</h1>
+                        <br />
+                        <p>
+                            <Link to="/home" relative="path">Ir a blogs</Link>
+                        </p>
+                    </section>
+                ) : (
+                    <section>
+                        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+                        <h1>Login</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="text"
+                                    id="email"
+                                    value={email}
+                                    ref={emailRef}
+                                    autoComplete="on"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                    className="input-field"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password">Password </label>
+                                <input
+                                    className="input-field"
+                                    type="password"
+                                    id="password"
+                                    value={password}
+                                    autoComplete="off"
+                                    required
+                                    onChange={(e) => setpassword(e.target.value)}
+                                />
+                            </div>
+                            <button className="submit-btn" type="submit">Log In</button>
                             <p>
-                                <Link to="/home" relative="path">Ir a blogs</Link>
+                                No tienes cuenta? <br />
+                                <span className="line">
+                                    <Link to="/signup" relative="path">Registrarse</Link>
+                                </span>
                             </p>
-                        </section>
-                    ) : (
-                        <section>
-                            <h1>Login</h1>
-                            <form onSubmit={this.handleSubmit}>
-                                <div>
-                                    <label htmlFor="email">Email</label>
-                                    <input
-                                        type="text"
-                                        id="email"
-                                        value={this.state.email}
-                                        autoComplete="on"
-                                        onChange={(e) => this.setState({ email: e.target.value })}
-                                        required
-                                        className="input-field"
-                                    />
-                                </div>
-                                <div>
-                                    <label htmlFor="password">Password </label>
-                                    <input
-                                        className="input-field"
-                                        type="password"
-                                        id="password"
-                                        value={this.state.password}
-                                        autoComplete="off"
-                                        required
-                                        onChange={(e) => this.setState({ password: e.target.value })}
-                                    />
-                                </div>
-                                <button className="submit-btn" type="submit">Log In</button>
-                                <p>
-                                    No tienes cuenta? <br />
-                                    <span className="line">
-                                        <Link to="/signup" relative="path">Registrarse</Link>
-                                    </span>
-                                </p>
-                            </form>
-                        </section>
-                    )}
-            </>
-        );
-    }
+                        </form>
+                    </section>
+                )}
+        </>
+    );
 
 }
 
