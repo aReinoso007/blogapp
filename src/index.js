b5c90b167f5f5e9a5605c93931df12eb38a6c23a
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2023-03-11 23:30:30 -0500

    auth provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import App from "./App";
 import { createRoot } from "react-dom/client"
+import React from "react";
+import { AuthProvider } from "./context/AuthProvider";
 
 const container = document.getElementById("root");
 const root = createRoot(container)
 root.render(
-  <App />
+ <React.StrictMode>
+   <AuthProvider>
+   <App />
+   </AuthProvider>
+ </React.StrictMode>
 );
